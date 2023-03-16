f60a7f5a94d24756a784815a12187ec7af6ffe94
nelson <nhigueraruiz@gmail.com>
2023-03-16 22:16:13 +0100

    removed data generators

--- clevr-graph/generate-station-arch-adj.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m gqa.generate --type-prefix StationArchitectureAdjacent --count 1000 --small --name saa-small-1m
--- clevr-graph/generate-station-shortest-count.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m gqa.generate --type-prefix StationShortestCount --count 1000 --small --name ssc-small-1m
