e40b239d67ef330aee37a1b56845a05b5b3c8fad
nelson <nhigueraruiz@gmail.com>
2023-03-19 00:31:14 +0100

    updated experiments sh

--- experiments.sh
+++ experiments.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 echo "SMALL" 
+python main.py -fp clevr-graph/small
 python main.py -fp clevr-graph/small -ocrgt True
 python main.py -fp clevr-graph/small -ogrgt True
 echo "MEDIUM" 
+python main.py -fp clevr-graph/medium
 python main.py -fp clevr-graph/medium -ocrgt True
 python main.py -fp clevr-graph/medium -ogrgt True
 echo "LARGE" 
+python main.py -fp clevr-graph/large
 python main.py -fp clevr-graph/large -ocrgt True
 python main.py -fp clevr-graph/large -ogrgt True
